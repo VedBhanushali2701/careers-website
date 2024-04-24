fed0197878cef381aab6f4a016ea0bf2194e062e
Ved Bhanushali <100978008+VedBhanushali2701@users.noreply.github.com>
2024-04-24 18:56:33 +0000

    Attempting to re-merge to main

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
   return render_template('home.html')
-
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
   # 0.0.0.0 makes it run on localhost
   # debug = True so that it automatically updates anytime we change the code
+
+
+
